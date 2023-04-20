a68b3117138b4da9fce26350080aad413a773d22
JaeHoon-Shin <sinhoon0805@naver.com>
2023-04-20 18:19:38 +0900

    내용정리

--- src/component/ProjectItem.js
+++ src/component/ProjectItem.js
@@ -46,6 +46,9 @@ const Projectinfo = ({ Slider }) => {
                   }
                 </Slider>
               </div>
+              <a href={`${info.url}`}
+              target="_blank" rel="noopener noreferrer">
+              {info.url}</a>
               <div className={styles.skillBox}>
                 {
                   info.skill && info.skill.split(',').map((obj, key) => {
@@ -54,16 +57,16 @@ const Projectinfo = ({ Slider }) => {
                 }
 
               </div>
-              <a href={`${info.url}`}
-                                    target="_blank" rel="noopener noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill='white' d="M6 17c2.269-9.881 11-11.667 11-11.667v-3.333l7 6.637-7 6.696v-3.333s-6.17-.171-11 5zm12 .145v2.855h-16v-12h6.598c.768-.787 1.561-1.449 2.339-2h-10.937v16h20v-6.769l-2 1.914z"/></svg>
-                                    Site View</a>
+              
             </div>
             <div className={styles.textBox}>
               <p><b>기획의도: </b>{info.plan && info.plan}</p>
               <p ><b>기능: </b>{info.function && info.function}</p>
+              {info.role ? <p><b>역할: </b>{info.role}</p>:<></>}
               <p><b>Review:</b>
                 {info && info.review}
               </p>
+              
             </div>
 
           </div>
